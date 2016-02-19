02eafff90414651417b1fb173c76f60ec8219beb
Xiaocheng Tang <xiaocheng.t@gmail.com>
2016-02-18 16:07:48 -0800

    remove comment

--- src/main/java/com/gopivotal/cf/workshop/Application.java
+++ src/main/java/com/gopivotal/cf/workshop/Application.java
@@ -9,7 +9,6 @@ import org.springframework.boot.SpringApplication;
 public class Application {
 
     public static void main(String[] args) {
-    	// add comment
         SpringApplication.run(Config.class, args);
     }
 
